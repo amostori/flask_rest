2eb1152a006970c138fe58b51884bbaf94b468fc
Marcin Andrzejczak <amostori@op.pl>
2022-12-08 23:30:24 +0100

    dockerfile entrypoint

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from db import db
 
 import models
 import os
+from dotenv import load_dotenv
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -17,6 +18,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
